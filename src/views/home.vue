24e70243570a5b803f487df4e20d080b52711947
lic <zheyuan54@qq.com>
2022-03-19 16:01:43 +0800

    feat: 初始化路由

---
+++ src/views/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    首页
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  reactive, toRefs, defineComponent,
+} from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {},
+  setup(props: any, context: any) {
+    const state = reactive({});
+    return {
+      ...toRefs(state),
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
