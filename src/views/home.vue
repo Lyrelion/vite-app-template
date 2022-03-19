4d61faa262234a6b959f723e9ecfdefbc0361b32
lic <zheyuan54@qq.com>
2022-03-19 16:24:13 +0800

    feat: 引入 elementplus；测试 vuex 数据修改

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="">
-    首页
+  <div>
+    <h2>首页</h2>
+    <br />
+    <span>{{ `通过 state 获取的 store 数据，没有及时更新 ${storeNum}` }}</span>
+    <br />
+    <span>{{ `直接使用 store 数据，可以及时更新 ${store.state.num}` }}</span>
+    <br />
+    <el-button @click="changeStore">测试 elementplus 组件库效果</el-button>
+    <br />
   </div>
 </template>
 
@@ -8,14 +15,26 @@
 import {
   reactive, toRefs, defineComponent,
 } from 'vue';
+import store from '../store'
 
 export default defineComponent({
   name: 'Home',
   components: {},
   setup(props: any, context: any) {
-    const state = reactive({});
+    const state = reactive({
+      storeNum: store.state.num,
+    });
+
+    const changeStore = () => {
+      console.log(store.state.num);
+      store.commit("addNum");
+      console.log(store.state.num);
+    };
+    
     return {
       ...toRefs(state),
+      changeStore,
+      store,
     };
   },
 });
